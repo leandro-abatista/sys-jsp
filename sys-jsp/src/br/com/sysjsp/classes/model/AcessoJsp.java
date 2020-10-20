c29b359ad2b8018cacbfdabc1c4c8088e5d16375
Leandro <Leandro@DESKTOP-SHKFPNB>
2020-10-20 10:22:25 -0300

    Classe de cadastro de usuários para acesso ao sistema.

---
+++ sys-jsp/src/br/com/sysjsp/classes/model/AcessoJsp.java
@@ -0,0 +1,87 @@
+package br.com.sysjsp.classes.model;
+
+public class AcessoJsp {
+
+	private Long id;
+	private String primeironome;
+	private String sobrenome;
+	private String ultimonome;
+	private String email;
+	private String telefone;
+	private String usuario;
+	private String senha;
+	private boolean ativo = true;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPrimeironome() {
+		return primeironome;
+	}
+
+	public void setPrimeironome(String primeironome) {
+		this.primeironome = primeironome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getUltimonome() {
+		return ultimonome;
+	}
+
+	public void setUltimonome(String ultimonome) {
+		this.ultimonome = ultimonome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
