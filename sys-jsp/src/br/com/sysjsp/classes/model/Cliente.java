bbf3f3fb5666292da0ea3472b0f38968d24ef68b
Leandro <Leandro@DESKTOP-SHKFPNB>
2020-10-23 16:44:48 -0300

    Implementando no projeto a classe cliente.

---
+++ sys-jsp/src/br/com/sysjsp/classes/model/Cliente.java
@@ -0,0 +1,87 @@
+package br.com.sysjsp.classes.model;
+
+public class Cliente {
+
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String orgaoexpeditor;
+	private String telefonefixo;
+	private String telefonecelular;
+	private String email;
+	private String observacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getOrgaoexpeditor() {
+		return orgaoexpeditor;
+	}
+
+	public void setOrgaoexpeditor(String orgaoexpeditor) {
+		this.orgaoexpeditor = orgaoexpeditor;
+	}
+
+	public String getTelefonefixo() {
+		return telefonefixo;
+	}
+
+	public void setTelefonefixo(String telefonefixo) {
+		this.telefonefixo = telefonefixo;
+	}
+
+	public String getTelefonecelular() {
+		return telefonecelular;
+	}
+
+	public void setTelefonecelular(String telefonecelular) {
+		this.telefonecelular = telefonecelular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+}
