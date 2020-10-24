a66b8ff98c3337d4f6f7fad9aa4eeb2e589a7875
Leandro <Leandro@DESKTOP-SHKFPNB>
2020-10-24 08:54:13 -0300

    Implementando a servlet do cliente.

--- sys-jsp/src/br/com/sysjsp/servlet/ServletCliente.java
+++ sys-jsp/src/br/com/sysjsp/servlet/ServletCliente.java
@@ -1,18 +1,30 @@
 package br.com.sysjsp.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.sysjsp.classes.model.Cliente;
+import br.com.sysjsp.dao.ClienteDao;
+
 /**
  * Servlet implementation class ServletCliente
  */
 @WebServlet("/ServletCliente")
 public class ServletCliente extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	
+	private ClienteDao clienteDao = new ClienteDao();
    
     public ServletCliente() {
         super();
@@ -24,6 +36,81 @@ public class ServletCliente extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String acao = request.getParameter("acao");
+		
+		if (acao != null && acao.equalsIgnoreCase("reset")) {
+			
+			RequestDispatcher view = request.getRequestDispatcher("/cadastroclientes.jsp");
+			request.setAttribute("clientes", clienteDao.listarTodos());
+			view.forward(request, response);
+			
+		} else {
+			
+			String id = request.getParameter("id");
+			String nome = request.getParameter("nome");
+			String cpf = request.getParameter("cpf");
+			String rg = request.getParameter("rg");
+			String orgaoexpeditor = request.getParameter("orgaoexpeditor");
+			String datanascimento = request.getParameter("datanascimento");
+			String telefonefixo = request.getParameter("telefonefixo");
+			String telefonecelular = request.getParameter("telefoneceluar");
+			String email = request.getParameter("email");
+			String observacao = request.getParameter("observaco");
+			
+			Cliente cliente = new Cliente();
+			cliente.setId(!id.isEmpty() ? Long.parseLong(id) : 0);
+			cliente.setNome(nome);
+			cliente.setCpf(cpf);
+			cliente.setRg(rg);
+			cliente.setOrgaoexpeditor(orgaoexpeditor);
+			try {
+				cliente.setDatanascimento(SimpleDateFormat.getDateInstance().parse(datanascimento));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			cliente.setTelefonefixo(telefonefixo);
+			cliente.setTelefonecelular(telefonecelular);
+			cliente.setEmail(email);
+			cliente.setObservacao(observacao);
+			
+			String msg = null;
+			boolean podeInserir = true;
+			
+			/*validação de nome e cpf*/
+			if (id == null || id.isEmpty() && !clienteDao.validarNomeCli(nome)) {
+				msg = "Já existe um cliente cadastrado com este nome!";
+				podeInserir = false;
+			} else
+			
+			if(id == null || id.isEmpty() && !clienteDao.validarCpfCli(cpf)) {
+				msg = "CPF já está cadastro para outro cliente. Por favor insira um CPF válido!";
+				podeInserir = false;
+			}
+			
+			/*verificando a mensagem se é nula ou não*/
+			if (msg != null) {
+				request.setAttribute("msg", msg);
+			}
+			
+			if (id == null || id.isEmpty() && clienteDao.validarCpfCli(cpf) && podeInserir) {
+				
+				clienteDao.salvarC(cliente);
+				msg = "Registro salvo com sucesso!";
+				
+			} else
+				
+			if (id != null || !id.isEmpty() && podeInserir) {
+				
+				clienteDao.updateC(cliente);
+				msg = "Registro atualizado com sucesso!";
+				
+			}
+			
+			RequestDispatcher view = request.getRequestDispatcher("/cadastroclientes.jsp");
+			request.setAttribute("clientes", clienteDao.listarTodos());
+			view.forward(request, response);
+			
+		}
 	}
 
 }
